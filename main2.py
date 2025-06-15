6559968b1569c16c5170d20d92d03ea2886cd5f4
ren <karenjw2003@gmail.com>
2025-06-15 17:40:35 +0700

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,6 +1,7 @@
 import discord
-import random
 from discord.ext import commands
+import os, random
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -24,14 +25,38 @@ async def repeat(ctx, times: int, content='repeating...'):
     """Repeats a message multiple times."""
     for i in range(times):
         await ctx.send(content)
-        
-@bot.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
 
 @bot.command()
-async def help(ctx):
-    await ctx.send("Menu help ini akan bantu kamu pakai bot.")
+async def mem(ctx):
+    img_name = random.choice(os.listdir('images'))
+    with open(f'images/{img_name}', 'rb') as f:
+        picture = discord.File(f)
+ 
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Setelah kita memanggil perintah bebek (duck), program akan memanggil fungsi get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+def get_dog_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('dog')
+async def duck(ctx):
+    image_url = get_dog_image_url()
+    await ctx.send(image_url)
+
 
 bot.run("")
